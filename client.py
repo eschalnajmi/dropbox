d4984bd10d0167e6c7a640684b27e419e1481292
eschalnajmi <eschal.najmi@gmail.com>
2024-10-22 09:42:31 +0100

    initial commit

---
+++ client.py
@@ -0,0 +1,138 @@
+import os
+import socket
+import hashlib
+import sys
+
+def getdir():
+    '''
+    Gets the source directory path from the user, if no path entered it returns the current working directory.
+    :return: source directory path
+    '''
+    if len(sys.argv) < 2:
+        source = os.getcwd()
+        os.chdir("../")
+        return source
+    
+    return sys.argv[1]
+
+def sendfiles(newfiles, source):
+    '''
+    Sends all new files to the server.
+    :param newfiles: list of new files
+    :param source: source directory path
+    :return: list of successfully added files
+    '''
+    successfullyadded = []
+    newcontents = [] # stores hash of new contents
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect(('0.0.0.0',8080))
+
+    for f in newfiles:
+        iterations = 1
+
+        client.send(f"{f}".encode())
+
+        server_msg = client.recv(4096).decode()
+        if server_msg != "Success":
+            print(f"Error with file {f}")
+            return []
+        
+        contents = open(os.path.join(source, f),"r").read()
+
+        if contents == "":
+            contents = f"\0"
+
+        if sys.getsizeof(contents) > 4096:
+            iterations = sys.getsizeof(contents)//4096 + 1
+
+        client.send(f"{iterations}".encode())
+        server_msg = client.recv(4096).decode()
+        if server_msg != "Success":
+            print(f"Error with file {f}")
+            return []
+        
+        client.send(f"{contents}".encode())
+        server_msg = client.recv(4096).decode()
+        if server_msg != "Success":
+            print(f"Error with file {f}")
+            return []
+        
+        successfullyadded.append(f)
+        newcontents.append(hashlib.md5(contents.encode()).hexdigest())
+
+    client.close()
+    return successfullyadded, newcontents
+
+def all_directories(path):
+    '''
+    Returns all directories in the path.
+    :param path: path to search for directories
+    :return: list of directories
+    '''
+    allfiles = []
+    curpath = path
+    for d in os.listdir(path):
+        if not(os.path.isdir(os.path.join(curpath, d))):
+            allfiles.append(os.path.join(curpath, d))
+        else:
+            allfiles+=all_directories(os.path.join(curpath, d))
+    return allfiles
+
+def getfiles(source):
+    '''
+    Gets all files from the source directory and sends any new ones to the server.
+    :param source: source directory path
+    '''
+    addedfiles=[]
+    addedcontents=[] # stores hash of added contents
+    dirpaths = []
+
+    for f in os.listdir(source):
+        if not(os.path.isdir(os.path.join(source, f))):
+            addedcontents.append(hashlib.md5(open(os.path.join(source, f),"r").read().encode()).hexdigest())
+        else:
+            dirs = all_directories(os.path.join(source,f))
+            for d in dirs:
+                addedcontents.append(hashlib.md5(open(d,"r").read().encode()).hexdigest())
+                dirpaths.append(d[(len(source)+1):])
+        
+
+    count = 0
+    while True:
+        allfilenames = [f for f in os.listdir(source) if not(os.path.isdir(os.path.join(source, f)))]
+        for f in os.listdir(source):
+            if os.path.isdir(os.path.join(source, f)):
+                dirs = all_directories(os.path.join(source,f))
+                for d in dirs:
+                    if d[(len(source)+1):] not in dirpaths:
+                        dirpaths.append(d[(len(source)+1):])
+
+        allfilenames+=dirpaths
+
+        newfiles = [f for f in allfilenames if f not in addedfiles]
+
+        if len(newfiles)!=0: print(f"New files: {newfiles}")
+
+        for i, f in enumerate(addedfiles):
+            if hashlib.md5(open(os.path.join(source, f),"r").read().encode()).hexdigest() != addedcontents[i]:
+                newfiles.append(f)
+                addedcontents.pop(i)
+                addedfiles.pop(i)
+
+        if len(newfiles)==0:
+            continue
+
+        try:
+            newlyaddedfiles,newcontents=sendfiles(newfiles, source)
+            addedfiles+=newlyaddedfiles
+            addedcontents+=newcontents
+            count+=1
+            
+        except Exception as e:
+            print(f"Error: {e}")
+            return
+
+
+if __name__ == "__main__":
+    source = getdir()
+    getfiles(source)
---
+++ server.py
@@ -0,0 +1,96 @@
+import os
+import socket
+import sys
+from threading import Thread
+
+def getdir():
+    '''
+    Gets the destination directory path from the user.
+    :return: destination directory path and list of files already in the directory
+    '''
+    addedfiles = []
+    if len(sys.argv) < 2:
+        destination = "destination"
+    else:
+        destination = sys.argv[1]
+
+    if not(os.path.exists(destination)):
+        os.mkdir(destination)
+        print(f"Created directory {destination}\n\n")
+    else:
+        for f in os.listdir(destination):
+            if not(os.path.isdir(os.path.join(destination, f))):
+                addedfiles.append(f)
+            
+    return destination, addedfiles
+
+def RunServer(destination, addedfiles, client, addr):
+    '''
+    Actual server code that receives files from the client.
+    :param destination: destination directory path
+    :param addedfiles: list of files already in the directory
+    :param client: client socket
+    :param addr: client address
+    '''
+    while True:
+        filename = client.recv(4096).decode()
+        if filename == "": break
+
+        if "/" in filename:
+            tempdestination = destination
+            dirs = filename.split("/")
+            for d in dirs[:-1]:
+                tempdestination = os.path.join(tempdestination, d)
+                if not(os.path.exists(tempdestination)):
+                    os.mkdir(tempdestination)
+                    print(f"Created directory {tempdestination}\n\n")
+
+        file = open(os.path.join(destination, filename),"w")
+        client.send("Success".encode())
+
+        iterations = int(client.recv(4096).decode())
+        client.send("Success".encode())
+    
+        contents = ""
+        for i in range(iterations):
+            recvcontents = client.recv(4096).decode()
+            if recvcontents != f"\0":
+                contents += recvcontents
+            else:
+                contents = f"\0"
+                break
+        
+        file.write(contents)
+        file.flush()
+        file.close()
+        
+        client.send("Success".encode())
+
+        addedfiles.append(filename)
+    client.close()
+
+def connect(destination,addedfiles):
+    '''
+    Connects to the client and receives files from the client.
+    :param destination: destination directory path
+    :param addedfiles: list of files already in the directory
+    '''
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(('0.0.0.0',8080))
+    server.listen(5)
+
+    while True:
+        try:
+            client, addr = server.accept()
+            thread = Thread(target=RunServer, args=(destination, addedfiles, client, addr))
+            thread.start()
+        except Exception as e:
+            print(f"Error: {e}")
+            break
+    
+    client.close()
+    thread.join()
+        
+if __name__ == "__main__":
+    destination, addedfiles = getdir()
+    connect(destination, addedfiles)
